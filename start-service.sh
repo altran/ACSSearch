57c9e834cfdd87bd5d7962e3851858e0a34154a6
Stig Lau <stig@lau.no>
2014-05-19 09:27:04 +0200

    Updated startup script

--- start-service.sh
+++ start-service.sh
@@ -13,15 +13,15 @@ repo="content/repositories/snapshots/com/altran"
 # Maven artifact location
 name=$A-$V
 path="$server/$repo/$A"
-version=`curl -u nkk:hund -s "$path/maven-metadata.xml" | grep "<version>" | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/"`
+version=`curl -s "$path/maven-metadata.xml" | grep "<version>" | sed "s/.*<version>\([^<]*\)<\/version>.*/\1/"`
 echo "Version $version"
-build=`curl -u nkk:hund -s "$path/$version/maven-metadata.xml" | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
+build=`curl -s "$path/$version/maven-metadata.xml" | grep '<value>' | head -1 | sed "s/.*<value>\([^<]*\)<\/value>.*/\1/"`
 jar="$A-$build.jar"
 url="$path/$version/$jar"
 
 # Download
 echo $url
-wget --user=username --password=password -O $JARFILE -q -N $url
+wget -O $JARFILE -q -N $url
 
 nohup java -jar $JARFILE &
 
